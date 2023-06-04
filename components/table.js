9bb58e9e840dbd579d544b3854d2904488b18440
Ocstudent01 <oochc345@gmail.com>
2023-06-04 00:06:00 -0500

    se mdifico form,table,index y se agrego success,bug,updateUserForm,updateUserForm,database

--- components/table.js
+++ components/table.js
@@ -1,3 +1,5 @@
+import data from '../database/data.json'
+
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
 export default function Table(){    
     return (
@@ -25,31 +27,41 @@ export default function Table(){
                 </tr>
             </thead>
             <tbody className="bg-gray-200">
-                <tr className="bg-gray-50 text-center">
+                {
+                    data.map((obj,i)=><Tr{...obj}key={i}/>)
+                }
+    
+            </tbody>
+        </table>
+    )
+
+    
+}
+
+function Tr({id,name,avatar,email,salary,date,status}){
+    //aqui vamos a separ la data que estaba dentro tbody
+    return (
+        <tr className="bg-gray-50 text-center">
                     <td className="px-16 py-2 flex flex-row items-center">
-                        <img src="" alt=""/>
-                        <span className="text-center ml-2 font-semibold">Orlando Chavez</span>
+                        <img src={avatar ||"#"} alt=""/>
+                        <span className="text-center ml-2 font-semibold">{name||"unknown"}</span>
                     </td>
                     <td className="px-14 py-2">
-                        <span>orlando@gmail.com</span>
+                        <span>{email||"unknown"}</span>
                     </td>
                     <td className="px-14 py-2">
-                        <span>$1000</span>
+                        <span>{salary||"unknown"}</span>
                     </td>
                     <td className="px-14 py-2">
-                        <span>11-09-1996</span>
+                        <span>{date||"unknown"}</span>
                     </td>
                     <td className="px-14 py-2">
-                        <button className="cursor"><span className="bg-green-500 text-white px-2 py-1 rounded-full" >Active</span></button>
+                        <button className="cursor"><span className="bg-green-500 text-white px-2 py-1 rounded-full" >{status||"unknown"}</span></button>
                     </td>
                     <td className="px-14 py-2 flex justify-around gap-5">
                         <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
                         <button className="cursor"><BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt></button>
                     </td>
                 </tr>
-            </tbody>
-        </table>
     )
-
-    
 }
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,19 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Table from '../components/table';
 import Form from '../components/form';
+import { useState } from 'react';
 
 import { BiUserPlus } from "react-icons/bi";
 
 export default function Home() {
+
+  const [visible,setVisible]= useState(false)
+
+  const handler = ()=>{
+    //setVisible(visible ? false : true) esta manera pero hay otra manera mas corta 
+    setVisible(!visible)
+  }
+  
   return (
     <section className={styles.container}>
       <Head>
@@ -16,18 +25,21 @@ export default function Home() {
       </Head>
 
       <main className="py-5">
-        <h1 className='text-xl md:text-5xl text-center font-bold py-10'>Employes Manayament</h1>
+        <h1 className='text-xl md:text-4xl text-center font-bold py-6'>Employes Manayament</h1>
         <div className='container mx-auto flex justify-between py-5 border-b'>
           <div className='left flex gap-3'>
-            <button className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:boder-indigo-500 hover:text-gray-800'>
+            <button onClick={handler} className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:boder-indigo-500 hover:text-gray-800'>
               Add Employee <span className='px-1'><BiUserPlus size={23}></BiUserPlus></span>
             </button>
           </div>
         </div>
+
         {/* collapsable Form*/}
-        <div className='container mx-auto py-5'>
-          <Form></Form>
-        </div>
+        
+        {/*<Form></Form>*/}
+        {visible ? <Form></Form>:<></>}
+        
+
         {/*table*/}
         <div className='container mx-auto'>
           <Table></Table>
